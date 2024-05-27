62e9ce301bb4e0d70eea27d875e6af226929ed2f
Allen Zheng <zhenga8533@gmail.com>
2024-05-26 20:52:25 -0400

    initialized Bazaar cog

---
+++ src/cogs/bazaar.py
@@ -0,0 +1,37 @@
+from discord.ext import commands
+from discord.ext.commands import Context
+import discord
+
+
+class Bazaar(commands.Cog, name="bazaar"):
+    def __init__(self, bot) -> None:
+        """
+        This is the constructor method for the Bazaar class.
+        
+        :param bot: The discord bot.
+        """
+
+        self.bot = bot
+
+    @commands.hybrid_command(
+        name="copper",
+        description="This is a testing command that does nothing.",
+    )
+    async def copper(self, context: Context) -> None:
+        """
+        This is a testing command that does nothing.
+
+        :param context: The application command context.
+        """
+
+        pass
+
+
+async def setup(bot) -> None:
+    """
+    This function will be called when the cog is loaded.
+    
+    :param bot: The discord bot.
+    """
+
+    await bot.add_cog(Bazaar(bot))
